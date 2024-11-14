0312936a0869beab61c77ed59f1f653c9bb3f3ba
Tobias Merkle <tgamerkle@gmail.com>
2024-11-14 14:55:26 -0500

    white

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -24,7 +24,7 @@ export default function NavBar() {
             {viteMode === 'production' ? (
                 <></>
             ) : viteMode === 'staging' ? (
-                <div className="flex gap-2 absolute bottom-4 left-4 text-3xl font-black text-yellow-300 z-50 animate-pulse bg-gradient-to-r from-orange-500 via-red-500 to-pink-500 bg-clip-text text-transparent drop-shadow-[0_0_15px_rgba(234,179,8,0.8)] transform hover:scale-110 transition-all duration-300 cursor-pointer rounded-xl border-4 border-yellow-300 border-opacity-50 p-2">
+                <div className="flex gap-2 absolute bottom-4 left-4 text-3xl font-black text-white z-50 animate-pulse bg-gradient-to-r from-orange-500 via-red-500 to-pink-500 bg-clip-text text-transparent drop-shadow-[0_0_15px_rgba(234,179,8,0.8)] transform hover:scale-110 transition-all duration-300 cursor-pointer rounded-xl border-4 border-yellow-300 border-opacity-50 p-2">
                     🚧 STAGING MODE 🚧
                     {nukeButton}
                 </div>
